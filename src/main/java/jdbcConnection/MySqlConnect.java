306cac1179191e422ca51e5c945959faeecf8681
Hendrik <h.mainzer@fh-bingen.de>
2014-01-14 15:24:24 +0100

    MySqlConnect implemented

---
+++ src/main/java/jdbcConnection/MySqlConnect.java
@@ -0,0 +1,91 @@
+package jdbcConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * ...
+ * 
+ * @author Hendrik Mainzer
+ * @version
+ */
+public final class MySqlConnect {
+
+    private static final String mySqlDriver = "com.mysql.jdbc.Driver";
+    private static String host, dbName, login, pw;
+
+    /**
+     * private constructor to make it impossible to create an instance of
+     * MySqlConnect.
+     */
+    private MySqlConnect() {
+    };
+
+    /**
+     * getConnection() returns an Connection-Object which can be used to submit
+     * queries to the MySql-Database.
+     * 
+     * @return Connection-Object
+     */
+    public static Connection getConnection() {
+	try {
+	    // This will load the MySQL driver, each DB has its own driver
+	    Class.forName(mySqlDriver);
+	    Connection connect = DriverManager.getConnection("jdbc:mysql://"
+		    + host + "/" + dbName + "?user=" + dbName + "_" + login
+		    + "&password=" + pw);
+	    return connect;
+	} catch (SQLException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	} catch (ClassNotFoundException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+	return null;
+    }
+
+    /**
+     * Sets the parameters for the access of the MySql-DB.
+     * 
+     * @param aHost
+     *            : address of the host
+     * @param aDbName
+     *            : name of the db
+     * @param aLogin
+     *            : login of the user, actual db-user is dbname_login, wbs-tool
+     *            user is login
+     * @param aPassword
+     *            : password of the user
+     */
+    public static void setDbConncetion(final String aHost,
+	    final String aDbName, final String aLogin, final String aPassword) {
+	MySqlConnect.host = aHost;
+	MySqlConnect.dbName = aDbName;
+	MySqlConnect.login = aLogin;
+	MySqlConnect.pw = aPassword;
+	SQLExecuter.closeConnection();
+    }
+
+    /**
+     * @return the host
+     */
+    public static String getHost() {
+	return host;
+    }
+
+    /**
+     * @return the dbName
+     */
+    public static String getDbName() {
+	return dbName;
+    }
+
+    /**
+     * @return the login
+     */
+    public static String getLogin() {
+	return login;
+    }
+}
--- src/main/java/jdbcConnection/SQLExecuter.java
+++ src/main/java/jdbcConnection/SQLExecuter.java
@@ -1,125 +1,99 @@
-
 package jdbcConnection;
+
 import java.sql.*;
 
-
 /**
- * Studienprojekt:	WBS
+ * Studienprojekt: WBS
  * 
- * Kunde:				Pentasys AG, Jens von Gersdorff
- * Projektmitglieder:	Andre Paffenholz, 
- * 						Peter Lange, 
- * 						Daniel Metzler,
- * 						Samson von Graevenitz
+ * Kunde: Pentasys AG, Jens von Gersdorff Projektmitglieder: Andre Paffenholz,
+ * Peter Lange, Daniel Metzler, Samson von Graevenitz
  * 
- * Führt SQL-Statements aus.
- * Hält ein Connection-Objekt auf dem SQL-Updates und SQL-Querys
- * ausgeführt werden können.
+ * Führt SQL-Statements aus. Hält ein Connection-Objekt auf dem SQL-Updates
+ * und SQL-Querys ausgeführt werden können.
  * 
  * @author Samson von Graevenitz
  * @version - 0.1 30.11.2010
  */
 public class SQLExecuter {
 
-	//Datenelement fürs Konnektor-Objekt
-	private static Connection theConn = null; 
-	
-	/**
-	 * Default-Konstruktor 
-	 */
-	public SQLExecuter(){
-	}
-	
-	/**
-	 * Stellt die Verbindung zur MDB her und speichert das Connection-Objekt
-	 *
-	 * @exception Exception Es wird ein Fehler verursacht, falls die Verbindung fehlschägt.
-	 * Dabei wird "no Connection" auf der Konsole ausgegeben
-	 */
-	private static void getConnection(){
-		try{
-		if(theConn == null) {
-			theConn = MDBConnect.getConnection();
-		}
-			
-		}catch(Exception e){
-			System.out.println("no Connection");
-			
-		}
-	}
-	
-	/**
-	 * Schließt die Verbindung zur MDB
-	 */
-	public static void closeConnection(){
-		try{
-			if(theConn != null) {
-				theConn.close();
-			}
-		}catch(SQLException e){
-            e.printStackTrace();
-        }
-		theConn = null;
-	}
-	
-	/**
-	 * Führt ein SQL-Statement aus - welches kein ResultSet benötigt
-	 * z.B. CREATE TABLE, INSERT INTO
-	 * @param sql SQL Statement als String
-	 */
-	public static void executeUpdate(String sql) throws SQLException{
-		getConnection();
-		Statement stmt;
-		try {
-			stmt = theConn.createStatement();
-			//stmt = theConn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
-			stmt.executeUpdate(sql);
-			theConn.commit();
-			stmt.close();
-		} catch (SQLException e) {
-			/*
-			 * Falls ein Arbeitspaket gelöscht wird und die Kaskadierung dies Verhindert,
-			 * so wird eine entsprechende SQLException geworfen. Diese wird abgefangen und der Fehler wird
-			 * an die WPOverview durchgereicht, wo er dann verarbeitet wird.
-			 * 
-			 * Bei anderen Exceptions soll der Fehler nach wie vor ausgegeben werden.
-			 */
-			
-			if(e.getErrorCode() == -1612){
-				throw new SQLException(e);
-			}
-			else{
-//				e.printStackTrace();
-				throw e;
-			}
-		}finally{
-//			closeConnection();
-		}
-	}
-	
-	
+    private static Connection openConnection = null;
 
-	/**
-	 * Führt ein SQL-Statement aus, das ein ResultSet zurückgibt z.B.:	SELECT FROM
-	 * 
-	 * @param sql SQL Statement als String
-	 * @return liefert ein ResultSet zurück
-	 * 
-	 * @exception Exception ein leeres ResultSet wird zurückgegeben und die Fehlermeldung ausgegeben
-	 * 
-	 */
-    public static ResultSet executeQuery(String sql){
-        getConnection();
-        Statement stmt;
-        ResultSet rs;
-        try {
-            stmt = theConn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
-            rs = stmt.executeQuery(sql);
-            return rs;
-        } catch (SQLException e) {
-            e.printStackTrace();
-//            this.closeConnection();
-        }
-        return null;
+    /**
+     * Default-Konstruktor
+     */
+    public SQLExecuter() {
     }
+
+    /**
+     * Opens a connection to the DB.
+     */
+    private static void openConnection() {
+	try {
+	    if (openConnection == null) {
+		openConnection = MySqlConnect.getConnection();
+	    }
+	} catch (Exception e) {
+	    System.err.println("no Connection");
+
+	}
+    }
+
+    /**
+     * Closes the connection to the db.
+     */
+    public static void closeConnection() {
+	try {
+	    if (openConnection != null) {
+		openConnection.close();
+	    }
+	} catch (SQLException e) {
+	    e.printStackTrace();
+	}
+	openConnection = null;
+    }
+
+    /**
+     * @param sql
+     *            SQL Statement as String
+     * @throws SQLException
+     */
+    public static void executeUpdate(final String sql) throws SQLException {
+	openConnection();
+	Statement stmt;
+	try {
+	    stmt = openConnection.createStatement();
+	    stmt.executeUpdate(sql);
+	    openConnection.commit();
+	    stmt.close();
+	} catch (SQLException e) {
+	    if (e.getErrorCode() == -1612) {
+		throw new SQLException(e);
+	    } else {
+		throw e;
+	    }
+	}
+    }
+
+    /**
+     * 
+     * @param sql
+     *            SQL Statement as String
+     * @return returns a ResultSet
+     */
+    public static ResultSet executeQuery(final String sql) {
+	openConnection();
+	Statement stmt;
+	ResultSet rs;
+	try {
+	    stmt = openConnection.createStatement(
+		    ResultSet.TYPE_SCROLL_INSENSITIVE,
+		    ResultSet.CONCUR_UPDATABLE);
+	    rs = stmt.executeQuery(sql);
+	    return rs;
+	} catch (SQLException e) {
+	    e.printStackTrace();
+	}
+	return null;
+    }
+
 }
