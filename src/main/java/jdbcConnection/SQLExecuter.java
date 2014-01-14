1fc07ff7ad24f886191b569200bbfb185ea806f9
Hendrik <h.mainzer@fh-bingen.de>
2014-01-14 17:20:07 +0100

    New Connect/Login GUI mostly done

--- src/main/java/jdbcConnection/SQLExecuter.java
+++ src/main/java/jdbcConnection/SQLExecuter.java
@@ -16,84 +16,83 @@ import java.sql.*;
  */
 public class SQLExecuter {
 
-    private static Connection openConnection = null;
+	private static Connection openConnection = null;
 
-    /**
-     * Default-Konstruktor
-     */
-    public SQLExecuter() {
-    }
-
-    /**
-     * Opens a connection to the DB.
-     */
-    private static void openConnection() {
-	try {
-	    if (openConnection == null) {
-		openConnection = MySqlConnect.getConnection();
-	    }
-	} catch (Exception e) {
-	    System.err.println("no Connection");
-
+	/**
+	 * Default-Konstruktor
+	 */
+	public SQLExecuter() {
 	}
-    }
 
-    /**
-     * Closes the connection to the db.
-     */
-    public static void closeConnection() {
-	try {
-	    if (openConnection != null) {
-		openConnection.close();
-	    }
-	} catch (SQLException e) {
-	    e.printStackTrace();
+	/**
+	 * Opens a connection to the DB.
+	 */
+	private static void openConnection() {
+		try {
+			if (openConnection == null) {
+				openConnection = MySqlConnect.getConnection();
+			}
+		} catch (Exception e) {
+			System.err.println("no Connection");
+		}
 	}
-	openConnection = null;
-    }
 
-    /**
-     * @param sql
-     *            SQL Statement as String
-     * @throws SQLException
-     */
-    public static void executeUpdate(final String sql) throws SQLException {
-	openConnection();
-	Statement stmt;
-	try {
-	    stmt = openConnection.createStatement();
-	    stmt.executeUpdate(sql);
-	    openConnection.commit();
-	    stmt.close();
-	} catch (SQLException e) {
-	    if (e.getErrorCode() == -1612) {
-		throw new SQLException(e);
-	    } else {
-		throw e;
-	    }
+	/**
+	 * Closes the connection to the db.
+	 */
+	public static void closeConnection() {
+		try {
+			if (openConnection != null) {
+				openConnection.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		openConnection = null;
 	}
-    }
 
-    /**
-     * 
-     * @param sql
-     *            SQL Statement as String
-     * @return returns a ResultSet
-     */
-    public static ResultSet executeQuery(final String sql) {
-	openConnection();
-	Statement stmt;
-	ResultSet rs;
-	try {
-	    stmt = openConnection.createStatement(
-		    ResultSet.TYPE_SCROLL_INSENSITIVE,
-		    ResultSet.CONCUR_UPDATABLE);
-	    rs = stmt.executeQuery(sql);
-	    return rs;
-	} catch (SQLException e) {
-	    e.printStackTrace();
+	/**
+	 * @param sql
+	 *            SQL Statement as String
+	 * @throws SQLException
+	 */
+	public static void executeUpdate(final String sql) throws SQLException {
+		openConnection();
+		Statement stmt;
+		try {
+			stmt = openConnection.createStatement();
+			stmt.executeUpdate(sql);
+			openConnection.commit();
+			stmt.close();
+		} catch (SQLException e) {
+			if (e.getErrorCode() == -1612) {
+				throw new SQLException(e);
+			} else {
+				throw e;
+			}
+		}
 	}
-	return null;
-    }
+
+	/**
+	 * 
+	 * @param sql
+	 *            SQL Statement as String
+	 * @return returns a ResultSet
+	 */
+	public static ResultSet executeQuery(final String sql) {
+		openConnection();
+		Statement stmt;
+		ResultSet rs;
+		try {
+			stmt = openConnection.createStatement(
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_UPDATABLE);
+			rs = stmt.executeQuery(sql);
+			return rs;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
 }
