c57e7881ea9a05e2f46f16641ba72a906f2c5d35
Hendrik <h.mainzer@fh-bingen.de>
2014-01-24 19:48:21 +0100

    EmployeesCalendarModel - functions fixed and tested

--- src/main/java/dbaccess/models/mysql/MySQLEmployeeCalendarModel.java
+++ src/main/java/dbaccess/models/mysql/MySQLEmployeeCalendarModel.java
@@ -46,12 +46,12 @@ public class MySQLEmployeeCalendarModel implements EmployeeCalendarModel {
     private Connection connection;
 
     @Override
-    public void addNewEmployeeCalendar(EmployeeCalendar empCal) {
+    public final void addNewEmployeeCalendar(final EmployeeCalendar empCal) {
         connection = SQLExecuter.getConnection();
         try {
             PreparedStatement stm = null;
 
-            String storedProcedure = "CALL employee_calendar_new (?,?,?,?,?,?)";
+            String storedProcedure = "CALL employee_calendar_new(?,?,?,?,?,?)";
 
             stm = connection.prepareStatement(storedProcedure);
             stm.setInt(1, empCal.getFid_emp());
@@ -60,30 +60,32 @@ public class MySQLEmployeeCalendarModel implements EmployeeCalendarModel {
             stm.setString(4, empCal.getDescription());
             stm.setBoolean(5, empCal.isAvailability());
             stm.setBoolean(6, empCal.isFull_time());
-            
+
             stm.execute();
-            
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     @Override
-    public List<EmployeeCalendar> getEmployeeCalendar() {
+    public final List<EmployeeCalendar> getEmployeeCalendar() {
         connection = SQLExecuter.getConnection();
         List<EmployeeCalendar> empCalList = new ArrayList<EmployeeCalendar>();
         try {
             ResultSet result = null;
             EmployeeCalendar employeeCalendar = null;
             Statement stm = connection.createStatement();
-            result = stm.executeQuery("CALL employee_calendar_select(NULL,NULL)");
+            result =
+                    stm.executeQuery("CALL "
+                            + "employee_calendar_select(NULL,NULL)");
             while (result.next()) {
                 employeeCalendar = EmployeeCalendar.fromResultSet(result);
                 empCalList.add(employeeCalendar);
             }
-           
+
             return empCalList;
-            
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -91,21 +93,21 @@ public class MySQLEmployeeCalendarModel implements EmployeeCalendarModel {
     }
 
     @Override
-    public EmployeeCalendar getEmployeeCalendar(int id) {
+    public final EmployeeCalendar getEmployeeCalendar(final int id) {
         connection = SQLExecuter.getConnection();
         EmployeeCalendar employeeCalendar = null;
         try {
             ResultSet result = null;
-            
+
             PreparedStatement stm = null;
 
-            String storedProcedure = "CALL employee_calendar_select (?,NULL)";
+            String storedProcedure = "CALL employee_calendar_select(?,NULL)";
 
             stm = connection.prepareStatement(storedProcedure);
             stm.setInt(1, id);
-     
-            result=stm.executeQuery();
-            
+
+            result = stm.executeQuery();
+
             if (result.next()) {
                 employeeCalendar = EmployeeCalendar.fromResultSet(result);
             }
@@ -118,22 +120,23 @@ public class MySQLEmployeeCalendarModel implements EmployeeCalendarModel {
     }
 
     @Override
-    public List<EmployeeCalendar> getEmployeeCalendarForFID(int fid) {
+    public final List<EmployeeCalendar>
+            getEmployeeCalendarForFID(final int fid) {
         connection = SQLExecuter.getConnection();
         List<EmployeeCalendar> empCalList = new ArrayList<EmployeeCalendar>();
         try {
             ResultSet result = null;
             EmployeeCalendar employeeCalendar = null;
-            
+
             PreparedStatement stm = null;
 
-            String storedProcedure = "CALL employee_calendar_select (?,true)";
+            String storedProcedure = "CALL employee_calendar_select(?,true)";
 
             stm = connection.prepareStatement(storedProcedure);
             stm.setInt(1, fid);
-     
-            result=stm.executeQuery();
-            
+
+            result = stm.executeQuery();
+
             while (result.next()) {
                 employeeCalendar = EmployeeCalendar.fromResultSet(result);
                 empCalList.add(employeeCalendar);
@@ -147,56 +150,26 @@ public class MySQLEmployeeCalendarModel implements EmployeeCalendarModel {
     }
 
     @Override
-    public List<EmployeeCalendar> getEmployeeCalendarInDateRange(Date from, Date to) {
+    public final List<EmployeeCalendar> getEmployeeCalendarInDateRange(
+            final Date from, final Date to) {
         connection = SQLExecuter.getConnection();
         List<EmployeeCalendar> empCalList = new ArrayList<EmployeeCalendar>();
         try {
-           
+
             ResultSet result = null;
             EmployeeCalendar employeeCalendar = null;
-            
+
             PreparedStatement stm = null;
 
-            String storedProcedure = "CALL employee_calendar_select_by_date (?,?,NULL)";
+            String storedProcedure =
+                    "CALL employee_calendar_select_by_date(?,?)";
 
             stm = connection.prepareStatement(storedProcedure);
             stm.setTimestamp(1, new Timestamp(from.getTime()));
             stm.setTimestamp(2, new Timestamp(to.getTime()));
-          
-            result=stm.executeQuery();
-            
-            while (result.next()) {
-                employeeCalendar = EmployeeCalendar.fromResultSet(result);
-                empCalList.add(employeeCalendar);
-            }
-            
-            return empCalList;
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-    
-    @Override
-    public List<EmployeeCalendar> getEmployeeCalendarInDateRange(Date from, Date to, boolean mode2) {
-        connection = SQLExecuter.getConnection();
-      
-        List<EmployeeCalendar> empCalList = new ArrayList<EmployeeCalendar>();
-        try {
-            ResultSet result = null;
-            EmployeeCalendar employeeCalendar = null;
-            
-            PreparedStatement stm = null;
 
-            String storedProcedure = "CALL employee_calendar_select_by_date (?,?,?)";
+            result = stm.executeQuery();
 
-            stm = connection.prepareStatement(storedProcedure);
-            stm.setTimestamp(1, new Timestamp(from.getTime()));
-            stm.setTimestamp(2, new Timestamp(to.getTime()));
-            stm.setBoolean(3, mode2);
-            
-            result=stm.executeQuery();
-            
             while (result.next()) {
                 employeeCalendar = EmployeeCalendar.fromResultSet(result);
                 empCalList.add(employeeCalendar);
@@ -208,22 +181,56 @@ public class MySQLEmployeeCalendarModel implements EmployeeCalendarModel {
         }
         return null;
     }
-    
+
     @Override
-    public void deleteEmployeeCalendar(int id) {
+    public final void deleteEmployeeCalendar(final int id) {
         connection = SQLExecuter.getConnection();
         try {
             PreparedStatement stm = null;
 
-            String storedProcedure = "CALL employee_calendar_delete_by_id (?)";
+            String storedProcedure = "CALL employee_calendar_delete_by_id(?)";
 
             stm = connection.prepareStatement(storedProcedure);
-            stm.setInt(1,id);
-          
+            stm.setInt(1, id);
+
             stm.execute();
-            
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public final List<EmployeeCalendar> getEmployeeCalendarForFIDInDateRange(
+            final int fid, final Date from, final Date to) {
+        connection = SQLExecuter.getConnection();
+        List<EmployeeCalendar> empCalList = new ArrayList<EmployeeCalendar>();
+        try {
+
+            ResultSet result = null;
+            EmployeeCalendar employeeCalendar = null;
+
+            PreparedStatement stm = null;
+
+            String storedProcedure =
+                    "CALL employee_calendar_select_by_date_and_emp(?,?,?)";
+
+            stm = connection.prepareStatement(storedProcedure);
+            stm.setTimestamp(1, new Timestamp(from.getTime()));
+            stm.setTimestamp(2, new Timestamp(to.getTime()));
+            stm.setInt(3, fid);
+
+            result = stm.executeQuery();
+
+            while (result.next()) {
+                employeeCalendar = EmployeeCalendar.fromResultSet(result);
+                empCalList.add(employeeCalendar);
+            }
+
+            return empCalList;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
--- src/test/java/dbaccess/models/mysql/MySQLEmployeeCalendarModelTest.java
+++ src/test/java/dbaccess/models/mysql/MySQLEmployeeCalendarModelTest.java
@@ -4,8 +4,12 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 import jdbcConnection.SQLExecuter;
@@ -20,7 +24,8 @@ import dbaccess.models.EmployeeCalendarModel;
 
 public class MySQLEmployeeCalendarModelTest {
     private EmployeeCalendarModel empCalModel;
-    private final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+    private final SimpleDateFormat dateFormat = new SimpleDateFormat(
+            "yyyy-mm-dd hh:mm:ss");
 
     @Before
     public final void setup() {
@@ -36,7 +41,6 @@ public class MySQLEmployeeCalendarModelTest {
     public final void testAddNewEmployeeCalendar() {
         // add a new employee calendar
 
-        
         EmployeeCalendar empCal = new EmployeeCalendar();
 
         try {
@@ -58,9 +62,15 @@ public class MySQLEmployeeCalendarModelTest {
             assertThat(empCalList, notNullValue());
             assertThat(empCalList.size(), equalTo(2));
             assertThat(empCalList.get(1).getFid_emp(), equalTo(1));
-            assertThat(dateFormat.parse(empCalList.get(1).getBegin_time().toString()), equalTo(dateFormat.parse("2014-01-13 00:00:00")));
-            assertThat(dateFormat.parse(empCalList.get(1).getEnd_time().toString()), equalTo(dateFormat.parse("2014-01-14 11:30:45")));
-            assertThat(empCalList.get(1).getDescription(), equalTo("Test Beschreibung"));
+            assertThat(
+                    dateFormat.parse(empCalList.get(1).getBegin_time()
+                            .toString()),
+                    equalTo(dateFormat.parse("2014-01-13 00:00:00")));
+            assertThat(dateFormat.parse(empCalList.get(1).getEnd_time()
+                    .toString()),
+                    equalTo(dateFormat.parse("2014-01-14 11:30:45")));
+            assertThat(empCalList.get(1).getDescription(),
+                    equalTo("Test Beschreibung"));
             assertThat(empCalList.get(1).isAvailability(), equalTo(true));
             assertThat(empCalList.get(1).isFull_time(), equalTo(true));
 
@@ -81,9 +91,14 @@ public class MySQLEmployeeCalendarModelTest {
             assertThat(empCalList, notNullValue());
             assertThat(empCalList.size(), equalTo(1));
             assertThat(empCalList.get(0).getFid_emp(), equalTo(3));
-            assertThat(dateFormat.parse(empCalList.get(0).getBegin_time().toString()), equalTo(dateFormat.parse("2014-01-13")));
-            assertThat(dateFormat.parse(empCalList.get(0).getEnd_time().toString()), equalTo(dateFormat.parse("2014-01-14")));
-            assertThat(empCalList.get(0).getDescription(), equalTo("Muss in Berufsschule"));
+            assertThat(
+                    dateFormat.parse(empCalList.get(0).getBegin_time()
+                            .toString()),
+                    equalTo(dateFormat.parse("2014-01-13")));
+            assertThat(dateFormat.parse(empCalList.get(0).getEnd_time()
+                    .toString()), equalTo(dateFormat.parse("2014-01-14")));
+            assertThat(empCalList.get(0).getDescription(),
+                    equalTo("Muss in Berufsschule"));
             assertThat(empCalList.get(0).isAvailability(), equalTo(false));
             assertThat(empCalList.get(0).isFull_time(), equalTo(true));
 
@@ -103,8 +118,10 @@ public class MySQLEmployeeCalendarModelTest {
 
             assertThat(empCal, notNullValue());
             assertThat(empCal.getFid_emp(), equalTo(3));
-            assertThat(dateFormat.parse(empCal.getBegin_time().toString()), equalTo(dateFormat.parse("2014-01-13")));
-            assertThat(dateFormat.parse(empCal.getEnd_time().toString()), equalTo(dateFormat.parse("2014-01-14")));
+            assertThat(dateFormat.parse(empCal.getBegin_time().toString()),
+                    equalTo(dateFormat.parse("2014-01-13")));
+            assertThat(dateFormat.parse(empCal.getEnd_time().toString()),
+                    equalTo(dateFormat.parse("2014-01-14")));
             assertThat(empCal.getDescription(), equalTo("Muss in Berufsschule"));
             assertThat(empCal.isAvailability(), equalTo(false));
             assertThat(empCal.isFull_time(), equalTo(true));
@@ -118,7 +135,8 @@ public class MySQLEmployeeCalendarModelTest {
     public final void testGetEmployeeCalendarForFID() {
         // get employee calendars with fid
 
-        List<EmployeeCalendar> empCalList = empCalModel.getEmployeeCalendarForFID(3);
+        List<EmployeeCalendar> empCalList =
+                empCalModel.getEmployeeCalendarForFID(3);
 
         try {
 
@@ -126,9 +144,14 @@ public class MySQLEmployeeCalendarModelTest {
             assertThat(empCalList, notNullValue());
             assertThat(empCalList.size(), equalTo(1));
             assertThat(empCalList.get(0).getFid_emp(), equalTo(3));
-            assertThat(dateFormat.parse(empCalList.get(0).getBegin_time().toString()), equalTo(dateFormat.parse("2014-01-13")));
-            assertThat(dateFormat.parse(empCalList.get(0).getEnd_time().toString()), equalTo(dateFormat.parse("2014-01-14")));
-            assertThat(empCalList.get(0).getDescription(), equalTo("Muss in Berufsschule"));
+            assertThat(
+                    dateFormat.parse(empCalList.get(0).getBegin_time()
+                            .toString()),
+                    equalTo(dateFormat.parse("2014-01-13")));
+            assertThat(dateFormat.parse(empCalList.get(0).getEnd_time()
+                    .toString()), equalTo(dateFormat.parse("2014-01-14")));
+            assertThat(empCalList.get(0).getDescription(),
+                    equalTo("Muss in Berufsschule"));
             assertThat(empCalList.get(0).isAvailability(), equalTo(false));
             assertThat(empCalList.get(0).isFull_time(), equalTo(true));
 
@@ -139,32 +162,73 @@ public class MySQLEmployeeCalendarModelTest {
 
     @Test
     public final void testGetEmployeeCalendarInDateRange() {
-        //get all employee calendars in a specific period
+        // get all employee calendars in a specific period
         List<EmployeeCalendar> empCalList;
         try {
-            empCalList = empCalModel.getEmployeeCalendarInDateRange(dateFormat.parse("2014-01-10 00:00:00"),dateFormat.parse("2014-01-15 00:00:00"));
+            empCalList =
+                    empCalModel.getEmployeeCalendarInDateRange(
+                            dateFormat.parse("2014-01-10 00:00:00"),
+                            dateFormat.parse("2014-01-15 00:00:00"));
             assertThat(empCalList, notNullValue());
-            assertThat(empCalList.size(),equalTo(1));
-            assertThat(empCalList.get(0).getFid_emp(),equalTo(3));
-            
+            assertThat(empCalList.size(), equalTo(1));
+            assertThat(empCalList.get(0).getFid_emp(), equalTo(3));
+
         } catch (ParseException e) {
             e.printStackTrace();
         }
     }
 
     @Test
-    public final void testGetEmployeeCalendarInDateRange1() {
-        //get all employee calendars in a specific period mode2
-        //TODO: was macht das
-        
+    public final void testGetEmployeeCalendarForFIDInDateRange() {
         List<EmployeeCalendar> empCalList;
         try {
-            empCalList = empCalModel.getEmployeeCalendarInDateRange(dateFormat.parse("2014-01-15 00:00:00"),dateFormat.parse("2014-01-20 00:00:00"),true);
-            assertThat(empCalList, notNullValue());
-            assertThat(empCalList.size(),equalTo(1));
-            assertThat(empCalList.get(0).getFid_emp(),equalTo(3));
-            
+            Timestamp from =
+                    new Timestamp(dateFormat.parse("2014-01-15 00:00:00")
+                            .getTime());
+            Timestamp to =
+                    new Timestamp(dateFormat.parse("2014-01-20 00:00:00")
+                            .getTime());
+
+            empCalList =
+                    empCalModel.getEmployeeCalendarForFIDInDateRange(2,
+                            dateFormat.parse("2014-01-15 00:00:00"),
+                            dateFormat.parse("2014-01-20 00:00:00"));
+
+            String statement =
+                    "SELECT * FROM " + "(SELECT * FROM employees_calendar "
+                            + "WHERE fid_emp = ?) AS tmp "
+                            + "WHERE (begin_time < ? AND end_time > ?) "
+                            + "OR (begin_time BETWEEN ? AND ? "
+                            + "OR end_time BETWEEN ? AND ?)";
+            java.sql.PreparedStatement stmt =
+                    SQLExecuter.getConnection().prepareStatement(statement);
+
+            stmt.setInt(1, 2);
+            stmt.setTimestamp(2, from);
+            stmt.setTimestamp(3, to);
+            stmt.setTimestamp(4, from);
+            stmt.setTimestamp(5, to);
+            stmt.setTimestamp(6, from);
+            stmt.setTimestamp(7, to);
+            ResultSet rslt = stmt.executeQuery();
+            List<EmployeeCalendar> ec = new ArrayList<EmployeeCalendar>();
+            if (rslt != null) {
+                while (rslt.next()) {
+                    ec.add(EmployeeCalendar.fromResultSet(rslt));
+                }
+            }
+            assertThat(empCalList.size(), equalTo(ec.size()));
+            if (empCalList.size() == ec.size()) {
+                for (int i = 0; i < empCalList.size(); i++) {
+                    assertThat(empCalList.get(i).getId(), equalTo(ec.get(i)
+                            .getId()));
+                }
+            }
+
         } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
@@ -173,9 +237,9 @@ public class MySQLEmployeeCalendarModelTest {
     public final void testDeleteEmployeeCalendar() {
         empCalModel.deleteEmployeeCalendar(1);
         List<EmployeeCalendar> empCalList = empCalModel.getEmployeeCalendar();
-        
-        assertThat(empCalList.size(),equalTo(0));
-        
+
+        assertThat(empCalList.size(), equalTo(0));
+
         TestData.reloadData(SQLExecuter.getConnection());
     }
 }
