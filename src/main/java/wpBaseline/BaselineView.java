e72b7dc82a7cac6dd5ca8420c7b807754506e601
Daniel <d.staudt@fh-bingen.de>
2014-02-08 13:59:46 +0100

    Checkstyle in package wpBaseline

--- src/main/java/wpBaseline/BaselineView.java
+++ src/main/java/wpBaseline/BaselineView.java
@@ -1,7 +1,23 @@
+/*
+ * The WBS-Tool is a project management tool combining the Work Breakdown
+ * Structure and Earned Value Analysis Copyright (C) 2013 FH-Bingen This
+ * program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your
+ * option) any later version. This program is distributed in the hope that
+ * it will be useful, but WITHOUT ANY WARRANTY;; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details. You should have received a
+ * copy of the GNU General Public License along with this program. If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+
 package wpBaseline;
 
-import de.fhbingen.wbs.translation.LocalizedStrings;
-import java.sql.ResultSet;
+import functions.WpManager;
+import globals.Controller;
+import globals.Workpackage;
+
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,62 +26,46 @@ import javax.swing.JFrame;
 
 import dbaccess.DBModelManager;
 import dbaccess.data.AnalyseData;
-import functions.WpManager;
-import globals.Controller;
-import globals.Workpackage;
-import jdbcConnection.SQLExecuter;
+import de.fhbingen.wbs.translation.LocalizedStrings;
 
 /**
- * Studienprojekt: PSYS WBS 2.0<br/>
- * Kunde: Pentasys AG, Jens von Gersdorff<br/>
- * Projektmitglieder:<br/>
- * Michael Anstatt,<br/>
- * Marc-Eric Baumgärtner,<br/>
- * Jens Eckes,<br/>
- * Sven Seckler,<br/>
- * Lin Yang<br/>
- * Funktionalitaet der BaselineViewGUI<br/>
- *
- * @author Michael Anstatt, Lin Yang
- * @version 2.0 - 20.08.2012
+ * Functionality of the BaselineViewGUI.
  */
 public class BaselineView {
     /**
-     * Konstruktor
-     *
+     * Constructor.
      * @param baselineID
-     *            ID der gewuenschten Baseline
+     *            id from the wished baseline
      * @param parent
      *            ParentFrame
      */
-    public BaselineView(int baselineID, JFrame parent) {
+    public BaselineView(final int baselineID, final JFrame parent) {
         try {
             String[][] data = getData(baselineID).toArray(new String[1][1]);
             BaselineViewGUI gui = new BaselineViewGUI(parent);
             for (String[] actualRow : data) {
                 gui.addRow(actualRow);
             }
-            ;
 
         } catch (SQLException e) {
             Controller.showError(LocalizedStrings.getErrorMessages()
-                    .baselineLoadingError());
+                .baselineLoadingError());
             e.printStackTrace();
         }
     }
 
     /**
-     * Fuellt die BaselineViewGUI mit Daten aus der DB
-     *
+     * Insert the data from the database into the BaselineViewGUI.
      * @param baselineID
-     *            ID der gewuenschten Baseline
-     * @return Liste mit StringArrays der Daten
+     *            id from the wished baseline
+     * @return A list with the data in form of a String-array
      * @throws SQLException
+     *             Throws an SQLException
      */
-    private List<String[]> getData(int baselineID) throws SQLException {
-        List<AnalyseData> data =
-                DBModelManager.getAnalyseDataModel().getAnalyseDataForBaseline(
-                        baselineID);
+    private List<String[]> getData(final int baselineID)
+        throws SQLException {
+        List<AnalyseData> data = DBModelManager.getAnalyseDataModel()
+            .getAnalyseDataForBaseline(baselineID);
         List<String[]> allData = new ArrayList<String[]>();
         for (AnalyseData ad : data) {
 
@@ -82,28 +82,26 @@ public class BaselineView {
                 actualData[i++] = Controller.DECFORM.format(ad.getAc());
                 actualData[i++] = Controller.DECFORM.format(ad.getEtc());
                 actualData[i++] = Controller.DECFORM.format(ad.getCpi());
-                actualData[i++] =
-                        Controller.DECFORM.format(ad.getBac_costs()) + " EUR";
-                actualData[i++] =
-                        Controller.DECFORM.format(ad.getAc_costs()) + " EUR";
-                actualData[i++] =
-                        Controller.DECFORM.format(ad.getEtc_costs()) + " EUR";
-                actualData[i++] =
-                        Controller.DECFORM.format(ad.getEac()) + " EUR";
-                actualData[i++] =
-                        Controller.DECFORM.format(ad.getEv()) + " EUR";
-                actualData[i++] =
-                        Controller.DECFORM.format(WpManager.calcTrend(
-                                ad.getEv(), ad.getAc_costs()));
-                actualData[i++] =
-                        Controller.DECFORM.format(ad.getPv()) + " EUR";
-                actualData[i++] =
-                        Controller.DECFORM.format(ad.getSv()) + " EUR";
+                actualData[i++] = Controller.DECFORM.format(ad
+                    .getBac_costs()) + " EUR";
+                actualData[i++] = Controller.DECFORM.format(ad
+                    .getAc_costs()) + " EUR";
+                actualData[i++] = Controller.DECFORM.format(ad
+                    .getEtc_costs()) + " EUR";
+                actualData[i++] = Controller.DECFORM.format(ad.getEac())
+                    + " EUR";
+                actualData[i++] = Controller.DECFORM.format(ad.getEv())
+                    + " EUR";
+                actualData[i++] = Controller.DECFORM.format(WpManager
+                    .calcTrend(ad.getEv(), ad.getAc_costs()));
+                actualData[i++] = Controller.DECFORM.format(ad.getPv())
+                    + " EUR";
+                actualData[i++] = Controller.DECFORM.format(ad.getSv())
+                    + " EUR";
                 actualData[i++] = Controller.DECFORM.format(ad.getSpi());
-                actualData[i++] =
-                        ""
-                                + WpManager.calcPercentComplete(ad.getBac(),
-                                        ad.getEtc(), ad.getAc());
+                actualData[i++] = ""
+                    + WpManager.calcPercentComplete(ad.getBac(),
+                        ad.getEtc(), ad.getAc());
                 allData.add(actualData);
             }
         }
--- src/main/java/wpBaseline/ValueCellRenderer.java
+++ src/main/java/wpBaseline/ValueCellRenderer.java
@@ -1,3 +1,17 @@
+/*
+ * The WBS-Tool is a project management tool combining the Work Breakdown
+ * Structure and Earned Value Analysis Copyright (C) 2013 FH-Bingen This
+ * program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your
+ * option) any later version. This program is distributed in the hope that
+ * it will be useful, but WITHOUT ANY WARRANTY;; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details. You should have received a
+ * copy of the GNU General Public License along with this program. If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+
 package wpBaseline;
 
 import java.awt.Color;
@@ -8,76 +22,89 @@ import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 
 import wpOverview.WPOverview;
+
 /**
- * Studienprojekt:	PSYS WBS 2.0<br/>
- * 
- * Kunde:		Pentasys AG, Jens von Gersdorff<br/>
- * Projektmitglieder:<br/>
- *			Michael Anstatt,<br/>
- *			Marc-Eric Baumgärtner,<br/>
- *			Jens Eckes,<br/>
- *			Sven Seckler,<br/>
- *			Lin Yang<br/>
- * 
- * Legt die Attribute der Value Zellen fest<br/>
- * 
- * @author Michael Anstatt
- * @version 2.0 - 20.08.2012
+ * Defines the attributes from the value cells.
  */
 public class ValueCellRenderer extends DefaultTableCellRenderer {
+    /** Constant serialized ID used for compatibility. */
+    private static final long serialVersionUID = 1L;
 
-	private static final long serialVersionUID = 1L;
-	private boolean right;
-	private boolean colored;
+    /**
+     * Sets the alignment of the label's contents along the X axis to:
+     * right, if true. Else left.
+     */
+    private boolean right;
 
-	public ValueCellRenderer(boolean right, boolean colored) {
-		this.right = right;
-		this.colored = colored;
-	}
+    /** Shows the labels in color, if true. Else in black and white. */
+    private boolean colored;
 
-	public Component getTableCellRendererComponent(JTable aTable, Object value, boolean aIsSelected, boolean aHasFocus, int aRow, int aColumn) {
-		Component renderer = super.getTableCellRendererComponent(aTable, value, aIsSelected, aHasFocus, aRow, aColumn);
+    /**
+     * Constructor.
+     * @param right
+     *            The alignment of the labels content.
+     * @param colored
+     *            Decides if the labels are shown in color or black and
+     *            white.
+     */
+    public ValueCellRenderer(final boolean right, final boolean colored) {
+        this.right = right;
+        this.colored = colored;
+    }
 
-		if (renderer instanceof JLabel) {
-			JLabel l = (JLabel) renderer;
-			if (right) {
-				l.setHorizontalAlignment(JLabel.RIGHT);
-			} else {
-				l.setHorizontalAlignment(JLabel.LEFT);
-			}
+    @Override
+    public final Component getTableCellRendererComponent(
+        final JTable aTable, final Object value, final boolean aIsSelected,
+        final boolean aHasFocus, final int aRow, final int aColumn) {
+        Component renderer = super.getTableCellRendererComponent(aTable,
+            value, aIsSelected, aHasFocus, aRow, aColumn);
 
-		}
+        if (renderer instanceof JLabel) {
+            JLabel l = (JLabel) renderer;
+            if (right) {
+                l.setHorizontalAlignment(JLabel.RIGHT);
+            } else {
+                l.setHorizontalAlignment(JLabel.LEFT);
+            }
 
-		if (!colored) {
-			if (aTable.getValueAt(aRow, 0).toString().substring(2, 3).equals(" ")) {
-				renderer.setBackground(Color.white);
-				renderer.setForeground(Color.black);
-			} else if (aTable.getValueAt(aRow, 0).toString().substring(1, 2).equals(" ")) {
-				renderer.setBackground(new Color(230, 230, 230));
-				renderer.setForeground(Color.black);
-			} else if (aTable.getValueAt(aRow, 0).toString().substring(0, 1).equals(" ")) {
-				renderer.setBackground(Color.LIGHT_GRAY);
-				renderer.setForeground(Color.black);
-			} else {
-				renderer.setBackground(Color.DARK_GRAY);
-				renderer.setForeground(Color.white);
-			}
+        }
 
-			if (aIsSelected) {
-				renderer.setBackground(Color.black);
-				renderer.setForeground(Color.white);
-			}
+        if (!colored) {
+            if (aTable.getValueAt(aRow, 0).toString().substring(2, 3)
+                .equals(" ")) {
+                renderer.setBackground(Color.white);
+                renderer.setForeground(Color.black);
+            } else if (aTable.getValueAt(aRow, 0).toString()
+                .substring(1, 2).equals(" ")) {
+                renderer.setBackground(new Color(230, 230, 230));
+                renderer.setForeground(Color.black);
+            } else if (aTable.getValueAt(aRow, 0).toString()
+                .substring(0, 1).equals(" ")) {
+                renderer.setBackground(Color.LIGHT_GRAY);
+                renderer.setForeground(Color.black);
+            } else {
+                renderer.setBackground(Color.DARK_GRAY);
+                renderer.setForeground(Color.white);
+            }
 
-		} else {
-			double doubleValue = Double.parseDouble(value.toString().replace(",", "."));
-			renderer.setForeground(WPOverview.getSPIColor(doubleValue, 1)[0]);
-			renderer.setBackground(WPOverview.getSPIColor(doubleValue, 1)[1]);
-			if (renderer instanceof JLabel) {
-				JLabel l = (JLabel) renderer;
-				l.setHorizontalAlignment(JLabel.RIGHT);
-			}
-		}
+            if (aIsSelected) {
+                renderer.setBackground(Color.black);
+                renderer.setForeground(Color.white);
+            }
 
-		return this;
-	}
+        } else {
+            double doubleValue = Double.parseDouble(value.toString()
+                .replace(",", "."));
+            renderer
+                .setForeground(WPOverview.getSPIColor(doubleValue, 1)[0]);
+            renderer
+                .setBackground(WPOverview.getSPIColor(doubleValue, 1)[1]);
+            if (renderer instanceof JLabel) {
+                JLabel l = (JLabel) renderer;
+                l.setHorizontalAlignment(JLabel.RIGHT);
+            }
+        }
+
+        return this;
+    }
 }
