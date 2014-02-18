3ba19e21d1441d986bca96fb89b1793b756fc05b
Maximilian Berger <snowdragon92@gmail.com>
2014-02-18 14:18:43 +0100

    javadocs and code style.

--- src/main/java/de/fhbingen/wbs/gui/wpBaseline/BaselineTableModel.java
+++ src/main/java/de/fhbingen/wbs/gui/wpBaseline/BaselineTableModel.java
@@ -7,8 +7,22 @@ import javax.swing.table.DefaultTableModel;
 * Created by Maxi on 18.02.14.
 */
 class BaselineTableModel extends DefaultTableModel {
-
+    /**
+     * UID for serializability.
+     */
     private static final long serialVersionUID = -7758000405864191565L;
+    /**
+     * Is editable values for each column.
+     */
+    private boolean[] columnEditables;
+
+    /** auto generated code from Eclipse. */
+    @SuppressWarnings("rawtypes")
+    private Class[] columnTypes;
+
+    /**
+     * Default constructor.
+     */
     public BaselineTableModel() {
         super(new Object[][]{{null, null, null, null, null, null, "",
                 null, "", null, null, null, null, null, null}, },
@@ -40,19 +54,18 @@ class BaselineTableModel extends DefaultTableModel {
                 false, false, false, false, false, false, false, false,
                 false, false, false};
     }
-
-    @SuppressWarnings("rawtypes")
-    // auto generated code from Eclipse
-    private Class[] columnTypes;
-
+    /** auto generated code from Eclipse */
     @SuppressWarnings({"unchecked", "rawtypes" })
-    // auto generated code from Eclipse
     public Class getColumnClass(final int columnIndex) {
         return columnTypes[columnIndex];
     }
 
-    private boolean[] columnEditables;
-
+    /**
+     * Checks if cell is editable.
+     * @param row row to check.
+     * @param column column to check.
+     * @return true if cell is editable.
+     */
     public boolean isCellEditable(final int row, final int column) {
         return columnEditables[column];
     }
