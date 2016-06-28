f301eef4fa3221f740f1c989a4e512be38d114b9
dimitri.babakow <dimitribabakow@gmail.com>
2016-06-28 19:03:45 +0200

    Update connection to REST API

--- src/main/java/de/fhbingen/wbs/timetracker/TimeTrackerConnector.java
+++ src/main/java/de/fhbingen/wbs/timetracker/TimeTrackerConnector.java
@@ -1,14 +1,22 @@
 package de.fhbingen.wbs.timetracker;
 
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.Charset;
-import org.json.simple.*;
-import org.json.simple.parser.JSONParser;
-
 
 /**
  * Connector to the application server.
@@ -18,30 +26,29 @@ public class TimeTrackerConnector {
     /**
      * URL for the application server.
      */
-    private String address;
+    private URL address;
 
     /**
      * Token for the login user.
      */
     private String token;
 
-    public TimeTrackerConnector(String address){
-        this.address = "http://" + address;
-
+    public TimeTrackerConnector(String address) throws MalformedURLException {
+        this.address = new URL(address + "/api/");
     }
 
     /**
      * Check the connection to the application server.
+     *
      * @return connection successful or not
      */
-    public boolean checkConnection(){
+    public boolean checkConnection() {
         HttpURLConnection connection = null;
         try {
-            URL url = new URL(this.address +"/api/");
-            connection = (HttpURLConnection) url.openConnection();
-            if(connection.getResponseCode() == HttpURLConnection.HTTP_BAD_GATEWAY)
+            connection = (HttpURLConnection) address.openConnection();
+            if (connection.getResponseCode() == HttpURLConnection.HTTP_BAD_GATEWAY)
                 return false;
-        } catch(Exception e){
+        } catch (IOException e) {
             return false;
         }
         return true;
@@ -50,143 +57,144 @@ public class TimeTrackerConnector {
 
     /**
      * Create new user.
+     *
      * @param name
      * @param password
      * @return the http response code
      */
-    public int createUser(String name, String password) {
-        HttpURLConnection connection = null;
-        try {
-            URL url = new URL(address + "/api/users/");
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setDoOutput(true);
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-type", "application/json");
+    public int createUser(String name, String password) throws IOException {
+        URL url = new URL(address, "users/");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setDoOutput(true);
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-type", "application/json");
 
-            JSONObject data = new JSONObject();
-            data.put("username", name);
-            data.put("password", password);
+        JSONObject data = new JSONObject();
+        data.put("username", name);
+        data.put("password", password);
 
-            OutputStreamWriter os = new OutputStreamWriter(connection.getOutputStream());
-            os.write(data.toString());
-            os.flush();
-            os.close();
+        OutputStreamWriter os = new OutputStreamWriter(connection.getOutputStream());
+        os.write(data.toString());
+        os.flush();
+        os.close();
 
-            return connection.getResponseCode();
+        connection.disconnect();
 
-        } catch (Exception e) {
-            e.printStackTrace();
-            return 0;
-        } finally {
-            connection.disconnect();
-        }
+        return connection.getResponseCode();
+
+
     }
 
     /**
      * It doesn't actually create a new project, it is just needed to update the backend, due to crappy legacy code.
      */
-    public int createProject(){
-        HttpURLConnection connection = null;
-        try {
-            URL url = new URL(address + "/api/projects/");
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setDoOutput(true);
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-type", "application/json");
-            connection.addRequestProperty("Authorization", "Token " + token);
+    public int createProject() throws IOException {
+        URL url = new URL(address, "projects/");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setDoOutput(true);
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-type", "application/json");
+        connection.addRequestProperty("Authorization", "Token " + token);
 
-            return connection.getResponseCode();
+        connection.disconnect();
 
-        } catch (Exception e) {
-            e.printStackTrace();
-            return 0;
-        } finally {
-            connection.disconnect();
-        }
+        return connection.getResponseCode();
+
 
     }
 
-    public void updateUser(){
-
+    /**
+     * Update or reset password.
+     * @param userID
+     * @param newPW
+     * @throws UnirestException
+     */
+    public void updateUser(int userID, String newPW) throws UnirestException {
+        HttpResponse<JsonNode> jsonResponse = Unirest.patch(address + "users/" + userID + "/")
+                .header("Conten-type", "application/json")
+                .header("Authorization", "Token " + token)
+                .field("password", newPW)
+                .asJson();
     }
 
-    public int loginUser(String name, String password){
-        HttpURLConnection connection = null;
-        try {
-            URL url = new URL(address + "/api/login/");
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setDoOutput(true);
-            connection.setDoInput(true);
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-type", "application/json");
+    /**
+     * Login user and get an token for authentication.
+     * @param name
+     * @param password
+     * @return
+     * @throws IOException
+     */
+    public int loginUser(String name, String password) throws IOException {
+        URL url = new URL(address, "login/");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setDoOutput(true);
+        connection.setDoInput(true);
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-type", "application/json");
 
-            JSONObject data = new JSONObject();
-            data.put("username", name);
-            data.put("password", password);
+        JSONObject data = new JSONObject();
+        data.put("username", name);
+        data.put("password", password);
 
-            OutputStreamWriter os = new OutputStreamWriter(connection.getOutputStream());
-            os.write(data.toString());
-            os.flush();
-            os.close();
+        OutputStreamWriter os = new OutputStreamWriter(connection.getOutputStream());
+        os.write(data.toString());
+        os.flush();
+        os.close();
 
-            if(connection.getResponseCode() != HttpURLConnection.HTTP_BAD_REQUEST) {
-                BufferedReader br = new BufferedReader(new InputStreamReader(
-                        connection.getInputStream(), Charset.forName("UTF-8")));
-                String jsonData = "";
-                String s;
-                while ((s = br.readLine()) != null) {
-                    jsonData += s;
-                }
-                br.close();
+        if (connection.getResponseCode() != HttpURLConnection.HTTP_BAD_REQUEST) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    connection.getInputStream(), Charset.forName("UTF-8")));
+            String jsonData = "";
+            String s;
+            while ((s = br.readLine()) != null) {
+                jsonData += s;
+            }
+            br.close();
+
+            try {
                 JSONParser jsParser = new JSONParser();
                 JSONObject jsObject = (JSONObject) jsParser.parse(jsonData);
                 this.token = jsObject.get("token").toString();
+            } catch(ParseException e){
+                e.printStackTrace();
             }
-
-
-            return connection.getResponseCode();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            return 0;
-        } finally {
-            connection.disconnect();
         }
+
+        connection.disconnect();
+
+        return connection.getResponseCode();
+
+
     }
 
-    public int addUserToProject(int dbID, int userID){
-        HttpURLConnection connection = null;
-        try {
-            URL url = new URL(address + "/api/users/" + userID + "/projects/");
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setDoOutput(true);
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-type", "application/json");
-            connection.addRequestProperty("Authorization", "Token " + token);
+    /**
+     * Align an project to user.
+     * @param dbID
+     * @param userID
+     * @return
+     * @throws IOException
+     */
+    public int addUserToProject(int dbID, int userID) throws IOException{
+        URL url = new URL(address, "users/" + userID + "/projects/");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setDoOutput(true);
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-type", "application/json");
+        connection.addRequestProperty("Authorization", "Token " + token);
 
-            JSONObject data = new JSONObject();
-            data.put("project", "/api/projects/" + dbID + "/");
+        JSONObject data = new JSONObject();
+        data.put("project", "/api/projects/" + dbID + "/");
 
-            OutputStreamWriter os = new OutputStreamWriter(connection.getOutputStream());
-            os.write(data.toString());
-            os.flush();
-            os.close();
+        OutputStreamWriter os = new OutputStreamWriter(connection.getOutputStream());
+        os.write(data.toString());
+        os.flush();
+        os.close();
 
-            System.out.println("Test");
+        connection.disconnect();
 
-            return connection.getResponseCode();
+        return connection.getResponseCode();
 
-        } catch (Exception e) {
-            e.printStackTrace();
-            return 0;
-        } finally {
-            connection.disconnect();
-        }
     }
-
-
-
-
 
 
 }
