d998bb402c5e0dae62484aacc45f4ace6145702f
dimitri.babakow <dimitribabakow@gmail.com>
2016-07-03 09:03:20 +0200

    Change Methods

--- src/main/java/de/fhbingen/wbs/timetracker/TimeTrackerConnector.java
+++ src/main/java/de/fhbingen/wbs/timetracker/TimeTrackerConnector.java
@@ -1,22 +1,19 @@
 package de.fhbingen.wbs.timetracker;
 
-
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import com.mashape.unirest.request.HttpRequestWithBody;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import java.io.BufferedReader;
+
+
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Connector to the application server.
@@ -56,146 +53,64 @@ public class TimeTrackerConnector {
         return true;
     }
 
-
     /**
-     * Create new user.
-     *
-     * @param name
-     * @param password
-     * @return the http response code
-     */
-    public int createUser(String name, String password) throws IOException {
-        URL url = new URL(address, "users/");
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setDoOutput(true);
-        connection.setRequestMethod("POST");
-        connection.setRequestProperty("Content-type", "application/json");
-
-        JSONObject data = new JSONObject();
-        data.put("username", name);
-        data.put("password", password);
-
-        OutputStreamWriter os = new OutputStreamWriter(connection.getOutputStream());
-        os.write(data.toString());
-        os.flush();
-        os.close();
-
-        connection.disconnect();
-
-        return connection.getResponseCode();
-
-
-    }
-
-    /**
-     * It doesn't actually create a new project, it is just needed to update the backend, due to crappy legacy code.
-     */
-    public int createProject() throws IOException {
-        URL url = new URL(address, "projects/");
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setDoOutput(true);
-        connection.setRequestMethod("POST");
-        connection.setRequestProperty("Content-type", "application/json");
-        connection.addRequestProperty("Authorization", "Token " + token);
-
-        connection.disconnect();
-
-        return connection.getResponseCode();
-
-
-    }
-
-    /**
-     * Update or reset password.
-     * @param userID
-     * @param newPW
+     * Make a [PATCH]
+     * @param url
+     * @param data
+     * @return response Code
      * @throws UnirestException
      */
-    public void updateUser(int userID, String newPW) throws UnirestException {
-        HttpResponse<JsonNode> jsonResponse = Unirest.patch(address + "users/" + userID + "/")
-                .header("Conten-type", "application/json")
-                .header("Authorization", "Token " + token)
-                .field("password", newPW)
-                .asJson();
+    public int patch(String url, Map<String, Object> data) throws UnirestException{
+        Map<String, String> header = new HashMap<>();
+        header.put("accept", "application/json");
+        header.put("Authorization", "Token " + token);
+
+        HttpRequestWithBody h = Unirest.patch(address + url);
+        h.headers(header);
+        h.fields(data);
+
+        return h.asJson().getCode();
     }
 
+
     /**
-     * Login user and get an token for authentication.
-     * @param name
-     * @param password
-     * @return
-     * @throws IOException
+     * Make a [POST]
+     * @param url
+     * @param data
+     * @param authentication
+     * @return resposne Code
+     * @throws UnirestException
      */
-    public int loginUser(String name, String password) throws IOException {
-        URL url = new URL(address, "login/");
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setDoOutput(true);
-        connection.setDoInput(true);
-        connection.setRequestMethod("POST");
-        connection.setRequestProperty("Content-type", "application/json");
+    public int post(String url, Map<String, Object> data, boolean authentication) throws UnirestException{
+        Map<String, String> header = new HashMap<>();
+        header.put("accept", "application/json");
 
-        JSONObject data = new JSONObject();
-        data.put("username", name);
-        data.put("password", password);
+        if(authentication){
+            header.put("Authorization", "Token " + token);
+        }
 
-        OutputStreamWriter os = new OutputStreamWriter(connection.getOutputStream());
-        os.write(data.toString());
-        os.flush();
-        os.close();
+        HttpRequestWithBody h = Unirest.post(address + url);
+        h.headers(header);
+        h.fields(data);
 
-        if (connection.getResponseCode() != HttpURLConnection.HTTP_BAD_REQUEST) {
-            BufferedReader br = new BufferedReader(new InputStreamReader(
-                    connection.getInputStream(), Charset.forName("UTF-8")));
-            String jsonData = "";
-            String s;
-            while ((s = br.readLine()) != null) {
-                jsonData += s;
-            }
-            br.close();
-
+        if(url.equals("login/")) {
             try {
-                JSONParser jsParser = new JSONParser();
-                JSONObject jsObject = (JSONObject) jsParser.parse(jsonData);
-                this.token = jsObject.get("token").toString();
-            } catch(ParseException e){
+                JSONObject j = h.asJson().getBody().getObject();
+                this.token = j.get("token").toString();
+            } catch (JSONException e) {
                 e.printStackTrace();
             }
         }
 
-        connection.disconnect();
-
-        return connection.getResponseCode();
-
-
+        return h.asJson().getCode();
     }
 
-    /**
-     * Align an project to user.
-     * @param dbID
-     * @param userID
-     * @return
-     * @throws IOException
-     */
-    public int addUserToProject(int dbID, int userID) throws IOException{
-        URL url = new URL(address, "users/" + userID + "/projects/");
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setDoOutput(true);
-        connection.setRequestMethod("POST");
-        connection.setRequestProperty("Content-type", "application/json");
-        connection.addRequestProperty("Authorization", "Token " + token);
+    public String getToken(){
+        return this.token;
+    }
 
-        JSONObject data = new JSONObject();
-        data.put("project", "/api/projects/" + dbID + "/");
-
-        OutputStreamWriter os = new OutputStreamWriter(connection.getOutputStream());
-        os.write(data.toString());
-        os.flush();
-        os.close();
-
-        connection.disconnect();
-
-        return connection.getResponseCode();
-
+    public void setToken(String token){
+        this.token = token;
     }
 
 
